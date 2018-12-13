18eefd567e4c86ad9471e9703a8e57019a6fecfa
Alicia Wilson <alicia@thearrayschool.com>
2018-12-13 15:11:30 -0700

    finished challenge 1

--- answer.js
+++ answer.js
@@ -1,1 +1,13 @@
 // *** YOUR ANSWER BELOW ***
+
+"use strict;"
+
+function beerSong(bottlesOfBeer){
+  let song = "";
+  let oneBeerDown = "";
+  for (let i = bottlesOfBeer; i >= 0; i--){
+    oneBeerDown = i - 1;
+    song += i + " bottles of beer on the wall " + "\n" + i + " bottles of beer." + "\n" + "You take one down pass it around " + "\n" + oneBeerDown + " bottles of beer on the wall \n";
+  }
+  return song;
+}
